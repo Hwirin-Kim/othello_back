427a1dd9249a71086ad7951bb7f5165e6474f856
Hwirin-Kim <kimhuirin@gmail.com>
2023-10-24 23:46:39 +0900

    feat : 로그인 기능 구현 / JWT토큰 발급까지 완성

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ import app from "./app";
 import initSocket from "./socket";
 import bcrypt from "bcryptjs";
 import { User } from "./model";
+import jwt from "jsonwebtoken";
 
 const server = createServer(app);
 const io = new Server(server, {
@@ -76,7 +77,7 @@ app.post("/register", async (req: Request, res: Response) => {
 app.post("/check", async (req, res) => {
   try {
     const { username } = req.body;
-    console.log(username);
+
     if (!username) {
       return res.status(400).json({ error: "username이 제공되지 않았습니다." });
     }
@@ -89,7 +90,62 @@ app.post("/check", async (req, res) => {
       res.status(200).json({ success: true, message: "사용 가능한 ID입니다." });
     }
   } catch (error) {
-    console.error("ID중복검사 오류 : ", error);
     res.status(500).json({ error: "서버 오류입니다." });
+  }
+});
+
+app.get("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    //미입력 필터링
+    if (!username || !password) {
+      return res.status(401).json({ error: "ID 또는 PW가 미입력 되었습니다." });
+    }
+
+    const user = await User.findOne({ where: { username } });
+    // 사용자가 DB에 존재하지 않는 경우
+    if (!user) {
+      return res.status(401).json({ error: "사용자를 찾을 수 없습니다." });
+    }
+
+    // 비밀번호 비교
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordMatch) {
+      const secretKey = process.env.JWT_SECRET;
+
+      const accessToken = jwt.sign({ username: user.username }, secretKey, {
+        expiresIn: "1h", // Access Token의 유효 기간 (1시간)
+      });
+
+      // Refresh Token 발급
+      const refreshToken = jwt.sign({ username: user.username }, secretKey, {
+        expiresIn: "7d", // Refresh Token의 유효 기간 (7일)
+      });
+
+      // Access Token을 'Authorization' 헤더에 추가하여 응답
+      res.setHeader("Authorization", `Bearer ${accessToken}`);
+
+      // HttpOnly 쿠키에 Refresh Token 저장
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true, // HttpOnly 속성을 설정하여 JavaScript로 접근을 방지
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 쿠키의 만료 기간 (7일)
+      });
+
+      // Refresh Token은 안전한 저장소에 저장 (예: HttpOnly 쿠키 또는 브라우저 저장소)
+      // 여기서는 예시로 클라이언트 응답 데이터에 추가합니다.
+      return res.status(200).json({
+        message: "로그인 성공!",
+        nickname: user.nickname,
+        username: user.username,
+      });
+    } else {
+      // 인증 실패
+      return res.status(401).json({ error: "비밀번호가 일치하지 않습니다." });
+    }
+  } catch (error) {
+    console.error("로그인 중 오류 발생:", error);
+    res.status(500).json({ error: "서버 오류" });
   }
 });
