26772883f390b4090ae28b53c84f7b34604f38a5
Hwirin-Kim <kimhuirin@gmail.com>
2023-11-03 17:22:12 +0900

    refactor : socket.ts에서 각 controller로 관심사 분리

--- controller/roomController.ts
+++ controller/roomController.ts
@@ -1,5 +1,7 @@
 import { Server } from "socket.io";
+import Message from "../models/domain/message";
 import Room from "../models/domain/room";
+import User from "../models/domain/user";
 
 export class RoomController {
   private io: Server;
@@ -14,5 +16,110 @@ export class RoomController {
     this.rooms.unshift(room);
     this.io.emit("room_list", this.rooms);
     return room;
+  }
+
+  joinRoom(roomId: string, user: User, socket) {
+    const room = this.rooms.find((room) => room.roomId === roomId);
+    const { username, nickname } = user;
+    console.log("joined 콘솔로그 ", room);
+    if (!room) {
+      return null;
+    }
+
+    const currentUserCount = room.users.length;
+    if (
+      currentUserCount > 2 &&
+      !room.users.some((user) => user.username === username)
+    ) {
+      console.log("room 정원 초과");
+      socket.emit("joined_failed", {
+        success: false,
+        data: "정원이 초과되었습니다.",
+      });
+      return true;
+    } else if (!room.users.some((user) => user.username === username)) {
+      console.log(nickname, ": 님 이 방에 접속함");
+      const userJoinedMessage = new Message(
+        "notice",
+        username,
+        nickname,
+        `${nickname} 님이 접속 하셨습니다.`
+      );
+      room.users.push(user);
+      socket.join(roomId);
+      socket.emit("joined_room", { success: true, data: roomId });
+      this.io.to(roomId).emit("user_joined", {
+        success: true,
+        data: userJoinedMessage,
+      });
+      return true;
+    } else {
+      console.log(nickname, ": 님 이 방에 접속함");
+      socket.join(roomId);
+      socket.emit("joined_room", { success: true, data: roomId });
+      return true;
+    }
+  }
+
+  sendMessage(data, user, socket) {
+    console.log(data);
+    const { username, nickname } = user;
+    const { roomId, message } = data;
+    const room = this.rooms.find((room) => room.roomId === roomId);
+    const sendMessage = new Message("message", username, nickname, message);
+
+    if (!room.users.some((user) => user.username === username)) {
+      console.log("해당 방에 접속하지 않았습니다.");
+      socket.emit("send_message", {
+        success: false,
+        data: "해당 방에 접속하지 못했습니다.",
+      });
+    } else {
+      this.io.to(roomId).emit("receive_message", {
+        success: true,
+        data: sendMessage,
+      });
+    }
+  }
+
+  leaveRoom(roomId, user, socket) {
+    const { username, nickname } = user;
+    const room = this.rooms.find((room) => room.roomId === roomId);
+    const leaveRoomMessage = new Message(
+      "notice",
+      username,
+      nickname,
+      `${nickname} 님이 방을 나가셨습니다.`
+    );
+
+    const leave = room.leaveUser(username);
+
+    if (leave) {
+      socket.leave(roomId);
+      console.log(nickname, ": 방나감");
+      this.io.to(roomId).emit("user_left", {
+        success: true,
+        data: leaveRoomMessage,
+      });
+
+      if (room.users.length === 0) {
+        this.deleteRoom(roomId);
+      }
+    } else {
+      console.log("해당 방에 접속하지 않았습니다.");
+      socket.emit("leave_room", {
+        success: false,
+        data: "해당 방에 접속하지 않으셨습니다.",
+      });
+    }
+  }
+
+  getRoom(socket) {
+    socket.emit("room_list", this.rooms);
+  }
+
+  deleteRoom(roomId: string) {
+    this.rooms = this.rooms.filter((room) => room.roomId !== roomId);
+    this.io.emit("room_list", this.rooms);
   }
 }
--- socket.ts
+++ socket.ts
@@ -5,112 +5,37 @@ import Message from "./models/domain/message";
 import Room from "./models/domain/room";
 import User from "./models/domain/user";
 
-const rooms: Room[] = [];
 export default function initSocket(io: Server) {
   const userController = new UserController(io);
   const roomController = new RoomController(io);
   io.on("connection", (socket) => {
     userController.connectUser(socket);
     const user = socket.data.user;
-
-    const { nickname, username } = user;
-
     socket.on("create_room", (title: string) => {
       const room = roomController.createRoom(title);
       socket.join(room.roomId);
       room.addUser(user);
       socket.emit("created_room", { success: true, roomId: room.roomId });
-      io.emit("room_list", rooms);
     });
     3;
 
-    socket.on("join_room", (roomId) => {
-      console.log(roomId, socket.id, "joined");
-      const room = rooms.find((room) => room.roomId === roomId);
-      console.log(room);
-      const currentUserCount = room.users.length;
-
-      if (
-        currentUserCount > 2 &&
-        !room.users.some((user) => user.username === username)
-      ) {
-        console.log("소켓 정원 초과");
-        socket.emit("joined_failed", {
-          success: false,
-          data: "방이 가득 찼습니다.",
-        });
-      } else if (!room.users.some((user) => user.username === username)) {
-        console.log(nickname, ": 님 이 방에 접속함");
-        const userJoinedMessage = new Message(
-          "notice",
-          username,
-          nickname,
-          `${nickname} 님이 접속 하셨습니다.`
-        );
-        room.users.push(user);
-        socket.join(roomId);
-        socket.emit("joined_room", { success: true, data: roomId });
-        io.to(roomId).emit("user_joined", {
-          success: true,
-          data: userJoinedMessage,
-        });
-      } else {
-        console.log(nickname, ": 님 이 방에 접속함");
-        socket.join(roomId);
-        socket.emit("joined_room", { success: true, data: roomId });
+    socket.on("join_room", (roomId, callback) => {
+      const join = roomController.joinRoom(roomId, user, socket);
+      if (!join) {
+        callback({ success: false, data: "해당 방을 찾을 수 없습니다." });
       }
-
-      console.log(room);
     });
 
     socket.on("send_message", (data) => {
-      console.log(data);
-      const { roomId, message } = data;
-      const room = rooms.find((room) => room.roomId === roomId);
-      const sendMessage = new Message("message", username, nickname, message);
-
-      if (!room.users.some((user) => user.username === username)) {
-        console.log("해당 방에 접속하지 않았습니다.");
-        socket.emit("send_message", {
-          success: false,
-          data: "해당 방에 접속하지 못했습니다.",
-        });
-      } else {
-        io.to(roomId).emit("receive_message", {
-          success: true,
-          data: sendMessage,
-        });
-      }
+      roomController.sendMessage(data, user, socket);
     });
 
     socket.on("leave_room", (roomId) => {
-      const room = rooms.find((room) => room.roomId === roomId);
-      const leaveRoomMessage = new Message(
-        "notice",
-        username,
-        nickname,
-        `${nickname} 님이 방을 나가셨습니다.`
-      );
-      if (!room.users.some((user) => user.username === username)) {
-        console.log("해당 방에 접속하지 않았습니다.");
-        socket.emit("leave_room", {
-          success: false,
-          data: "해당 방에 접속하지 않으셨습니다.",
-        });
-      } else {
-        room.users = room.users.filter((user) => user.username !== username);
-
-        socket.leave(roomId);
-        console.log(nickname, ": 방나감");
-        io.to(roomId).emit("user_left", {
-          success: true,
-          data: leaveRoomMessage,
-        });
-      }
+      roomController.leaveRoom(roomId, user, socket);
     });
 
     socket.on("get_room", () => {
-      socket.emit("room_list", rooms);
+      roomController.getRoom(socket);
     });
 
     socket.on("ready", (readyStatus) => {
