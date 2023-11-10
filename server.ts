d3f9cee45e585cf2a0c6cc9cbaa330cce374b3ce
Hwirin-Kim <kimhuirin@gmail.com>
2023-11-10 18:39:15 +0900

    fix : 각 model.ts파일에 정의되었던 연관관계를 index.js로 이동 각 모델은 더이상 서로를 참조하지 않음

--- server.ts
+++ server.ts
@@ -3,26 +3,18 @@ import { Server } from "socket.io";
 import app from "./app";
 import initSocket from "./socket";
 import { sequelize } from "./config/database";
-import userRoutes from "./routes/user.routes";
-import authRoutes from "./routes/auth.routes";
 import { User } from "./models/db/user.model";
 import { GameStatus } from "./models/db/status.model";
 import { Record } from "./models/db/record.model";
+import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
-
-    // 모델 동기화 순서를 제어
-    await User.sync();
-    console.log("User model was synchronized successfully.");
-
-    await GameStatus.sync();
-    console.log("GameStatus model was synchronized successfully.");
-
-    await Record.sync();
-    console.log("GameRecord model was synchronized successfully.");
+    await sequelize.sync();
+    console.log("model was synchronized successfully");
   } catch (error) {
     console.error("Database connection failed:", error);
     process.exit(1);
@@ -55,42 +47,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//     return User.sync({ force: true });
-//   })
-//   .then(() => {
-//     return GameStatus.sync({ force: true });
-//   })
-//   .then(() => {
-//     return Record.sync({ force: true }).then(() => {
-//       return console.log("DB updated successfully.");
-//     });
-//   })
-//   .then(() => {
-//     console.log("All models were synchronized successfully.");
-//   })
-//   .catch((err) => {
-//     console.error("Unable to connect to the database:", err);
-//   });
-
-// const server = createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:3000", "http://172.30.1.65:3000"],
-//     credentials: true,
-//   },
-// });
-
-// initSocket(io);
-
-// server.listen(8080, () => {
-//   console.log("server running at http://localhost:8080");
-// });
-
-// app.use("/user", userRoutes);
-
-// app.use("/", authRoutes);
