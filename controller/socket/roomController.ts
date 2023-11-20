8c01326dfdb054c717890b54e6901b171df4dee1
Hwirin-Kim <kimhuirin@gmail.com>
2023-11-20 22:21:06 +0900

    feat : 에러처리 추가

--- controller/socket/roomController.ts
+++ controller/socket/roomController.ts
@@ -16,73 +16,83 @@ export class RoomController {
 
   //방생성
   createRoom(title: string, socket: Socket, user: SimpleUser) {
-    const room = new Room(title);
-    this.rooms.unshift(room);
-    this.io.emit("room_list", this.rooms);
-    socket.join(room.roomId);
-    user.setStoneColor("white");
-    room.addUser(user);
-    return room;
+    try {
+      const room = new Room(title);
+      this.rooms.unshift(room);
+      this.io.emit("room_list", this.rooms);
+      socket.join(room.roomId);
+      user.setStoneColor("white");
+      room.addUser(user);
+      return room;
+    } catch (error) {
+      console.error(`RoomController - createRoom에러 :${error}`);
+      socket.emit("error", "방을 생성하는 데 실패했습니다.");
+      return null;
+    }
   }
 
   //방입장
   joinRoom(roomId: string, socket, simpleUser) {
-    const room = this.getRoom(roomId);
+    try {
+      const room = this.getRoom(roomId);
+      const { username, nickname } = simpleUser;
+      //방이 존재하지 않음
+      if (!room) {
+        console.log("방없음");
+        return {
+          success: false,
+          data: "해당 방을 찾을 수 없습니다.",
+        };
+      }
+      const MAXIMUM_USER = 2;
+      //방에 내가 없으면서 인원이 2명인 경우
+      const currentUserCount = room.users.length;
+      if (
+        currentUserCount >= MAXIMUM_USER &&
+        !room.users.some((user) => user.username === username)
+      ) {
+        console.log("정원초과");
+        return {
+          success: false,
+          data: "정원이 초과되었습니다.",
+        };
+      }
 
-    const { username, nickname } = simpleUser;
+      // 방에 처음 입장하는 경우
+      else if (!room.users.some((user) => user.username === username)) {
+        //상대의 색상과 반대의 돌 색상 할당
+        const opponent = room.users.find((u) => u.username !== username);
+        const myColor = opponent.stoneColor === "white" ? "black" : "white";
+        simpleUser.setStoneColor(myColor);
 
-    //방이 존재하지 않음
-    if (!room) {
-      console.log("방없음");
-      return {
-        success: false,
-        data: "해당 방을 찾을 수 없습니다.",
-      };
-    }
-    const MAXIMUM_USER = 2;
-    //방에 내가 없으면서 인원이 2명인 경우
-    const currentUserCount = room.users.length;
-    if (
-      currentUserCount >= MAXIMUM_USER &&
-      !room.users.some((user) => user.username === username)
-    ) {
-      console.log("정원초과");
-      return {
-        success: false,
-        data: "정원이 초과되었습니다.",
-      };
-    }
+        console.log(nickname, ": 님 이 방에 접속함");
+        const userJoinedMessage = new Message(
+          "notice",
+          username,
+          nickname,
+          `${nickname} 님이 접속 하셨습니다.`
+        );
+        room.users.push(simpleUser);
+        socket.join(roomId);
 
-    // 방에 처음 입장하는 경우
-    else if (!room.users.some((user) => user.username === username)) {
-      //상대의 색상과 반대의 돌 색상 할당
-      const opponent = room.users.find((u) => u.username !== username);
-      const myColor = opponent.stoneColor === "white" ? "black" : "white";
-      simpleUser.setStoneColor(myColor);
-
-      console.log(nickname, ": 님 이 방에 접속함");
-      const userJoinedMessage = new Message(
-        "notice",
-        username,
-        nickname,
-        `${nickname} 님이 접속 하셨습니다.`
-      );
-      room.users.push(simpleUser);
-      socket.join(roomId);
-
-      this.io.to(roomId).emit("user_joined", {
-        success: true,
-        data: userJoinedMessage,
-      });
-      return { success: true };
-    }
-    // 방에 재입장 하는 경우
-    else {
-      room.users = room.users.filter((user) => user.username !== username);
-      room.users.push(simpleUser);
-      console.log(nickname, ": 님 이 방에 접속함");
-      socket.join(roomId);
-      return { success: true };
+        this.io.to(roomId).emit("user_joined", {
+          success: true,
+          data: userJoinedMessage,
+        });
+        return { success: true };
+      }
+      // 방에 재입장 하는 경우
+      else {
+        room.users = room.users.filter((user) => user.username !== username);
+        room.users.push(simpleUser);
+        console.log(nickname, ": 님 이 방에 접속함");
+        socket.join(roomId);
+        return { success: true };
+      }
+    } catch (error) {
+      console.error(`RoomController-joinRoom에러 :${error}`);
+      socket.emit("error", "방에 참가하는데 실패했습니다.");
+      return null;
     }
   }
 
@@ -94,7 +104,7 @@ export class RoomController {
     const room = this.getRoom(roomId);
     const sendMessage = new Message("message", username, nickname, message);
 
-    if (!room.users.some((user) => user === username)) {
+    if (!room.users.some((user) => user.username === username)) {
       console.log("해당 방에 접속하지 않았습니다.");
       socket.emit("send_message", {
         success: false,
@@ -110,58 +120,64 @@ export class RoomController {
 
   //방퇴장
   leaveRoom(roomId, socket) {
-    const { username, nickname } = socket.user;
-    const room = this.getRoom(roomId);
-    const leaveRoomMessage = new Message(
-      "notice",
-      username,
-      nickname,
-      `${nickname} 님이 방을 나가셨습니다.`
-    );
+    try {
+      const { username, nickname } = socket.user;
+      const room = this.getRoom(roomId);
+      const leaveRoomMessage = new Message(
+        "notice",
+        username,
+        nickname,
+        `${nickname} 님이 방을 나가셨습니다.`
+      );
 
-    const leave = room.leaveUser(username);
-    const leftUser = room.leftUser();
+      const leave = room.leaveUser(username);
+      const leftUser = room.leftUser();
 
-    //room객체에서 user제거 시킨 후 실제 socket에서 제거시키기
-    if (leave) {
-      socket.leave(roomId);
-      console.log(nickname, ": 방나감");
-      this.io.to(roomId).emit("user_left", {
-        success: true,
-        data: leaveRoomMessage,
-      });
-
-      //남은 유저에게 방장 위임
-      if (leftUser && leftUser !== room.owner) {
-        room.setOwner(leftUser);
-        const message = new Message(
-          "notice",
-          username,
-          nickname,
-          `${leftUser.nickname}님이 방장이 되셨습니다.`
-        );
-        this.io.to(roomId).emit("receive_message", {
+      //room객체에서 user제거 시킨 후 실제 socket에서 제거시키기
+      if (leave) {
+        socket.leave(roomId);
+        console.log(nickname, ": 방나감");
+        this.io.to(roomId).emit("user_left", {
           success: true,
-          data: message,
+          data: leaveRoomMessage,
         });
-        this.emitRoomInfo(roomId);
-      }
 
-      //방 삭제
-      if (!leftUser) {
-        this.deleteRoom(roomId);
+        //남은 유저에게 방장 위임
+        if (leftUser && leftUser !== room.owner) {
+          room.setOwner(leftUser);
+          const message = new Message(
+            "notice",
+            username,
+            nickname,
+            `${leftUser.nickname}님이 방장이 되셨습니다.`
+          );
+          this.io.to(roomId).emit("receive_message", {
+            success: true,
+            data: message,
+          });
+          this.emitRoomInfo(roomId);
+        }
+
+        //방 삭제
+        if (!leftUser) {
+          this.deleteRoom(roomId);
+        }
+      } else {
+        console.log("해당 방에 접속하지 않았습니다.");
+        socket.emit("leave_room", {
+          success: false,
+          data: "해당 방에 접속하지 않으셨습니다.",
+        });
       }
-    } else {
-      console.log("해당 방에 접속하지 않았습니다.");
-      socket.emit("leave_room", {
-        success: false,
-        data: "해당 방에 접속하지 않으셨습니다.",
-      });
+    } catch (error) {
+      console.error(`RoomController-leaveRoom에러 :${error}`);
+      socket.emit("error", "방에 나가는데 실패했습니다.");
+      return null;
     }
   }
 
   //방목록전송(소켓이 클라이언트로)
-  getRoomList(socket) {
+  emitRoomList(socket) {
     socket.emit("room_list", this.rooms);
   }
 
@@ -172,8 +188,12 @@ export class RoomController {
   }
 
   //방 정보 가져오기
-  getRoom(roomId) {
-    return this.rooms.find((room) => room.roomId === roomId);
+  getRoom(roomId: string) {
+    const room = this.rooms.find((room) => room.roomId === roomId);
+    if (!room) {
+      console.error("RoomController - getRoom에러 : 해당 room이 없습니다.");
+      return null;
+    } else return room;
   }
 
   //방 정보 전송 (소켓이 클라이언트로)
@@ -184,53 +204,73 @@ export class RoomController {
 
   //사용자 레디
   ready(roomId: string, user: SimpleUser) {
-    const { username, nickname } = user;
-    const room = this.getRoom(roomId);
-    const ready = room.setReady(user);
+    try {
+      const { username, nickname } = user;
+      const room = this.getRoom(roomId);
+      const ready = room.setReady(user);
 
-    if (ready) {
-      const message = new Message(
-        "notice",
-        username,
-        nickname,
-        `${nickname}님이 준비 완료 하셨습니다.`
-      );
-      this.io
-        .to(roomId)
-        .emit("receive_message", { success: true, data: message });
-    } else {
-      const message = new Message(
-        "notice",
-        username,
-        nickname,
-        `${nickname}님이 준비를 취소하였습니다.`
-      );
-      this.io
-        .to(roomId)
-        .emit("receive_message", { success: true, data: message });
-    }
-    return ready;
-  }
-
-  async gameStart(roomId: string) {
-    const room = this.getRoom(roomId);
-    room.setRoomStatus("playing");
-    this.emitRoomInfo(roomId);
-    const winner = await room.startTurnTimer(this.io, roomId);
-
-    if (winner) {
-      this.gameOver(roomId);
-    }
-  }
-
-  async playerMove(roomId: string) {
-    const room = this.getRoom(roomId);
-    room.changeTurn();
-    this.emitTurn(roomId);
-    const winner = await room.startTurnTimer(this.io, roomId);
-    if (winner) {
-      this.gameOver(roomId);
+      if (ready) {
+        const message = new Message(
+          "notice",
+          username,
+          nickname,
+          `${nickname}님이 준비 완료 하셨습니다.`
+        );
+        this.io
+          .to(roomId)
+          .emit("receive_message", { success: true, data: message });
+      } else {
+        const message = new Message(
+          "notice",
+          username,
+          nickname,
+          `${nickname}님이 준비를 취소하였습니다.`
+        );
+        this.io
+          .to(roomId)
+          .emit("receive_message", { success: true, data: message });
+      }
+      return ready;
+    } catch (error) {
+      console.error(`RoomController-ready에러 : ${error}`);
       return null;
+    }
+  }
+
+  async gameStart(roomId: string, user: SimpleUser) {
+    try {
+      const room = this.getRoom(roomId);
+      room.setRoomStatus("playing");
+      this.emitRoomInfo(roomId);
+      await this.timerStart(roomId);
+    } catch (error) {
+      console.error(`RoomController - gameStart에러 : ${error}`);
+      return null;
+    }
+  }
+
+  async playerMove(roomId: string, user: SimpleUser) {
+    try {
+      const room = this.getRoom(roomId);
+      room.changeTurn();
+      this.emitTurn(roomId);
+      await this.timerStart(roomId);
+    } catch (error) {
+      console.error(`RoomController - playerMove에러 ${error}`);
+      return null;
+    }
+  }
+  async timerStart(roomId: string) {
+    try {
+      const room = this.getRoom(roomId);
+      const timeOver = await room.startTurnTimer(this.io, roomId);
+      if (timeOver) {
+        this.timeoutSetWinner(roomId);
+        this.gameOver(roomId);
+        return null;
+      }
+    } catch (error) {
+      console.error(`RoomController - timerStart에러 : ${error}`);
     }
   }
 
@@ -244,8 +284,24 @@ export class RoomController {
 
   gameOver(roomId: string) {
     const room = this.getRoom(roomId);
-    const winner = room.winner;
-    this.io.to(roomId).emit("game_over", winner);
+    const winnerStoneColor = room.winner;
+    const winner = room.users.find(
+      (user) => user.stoneColor === winnerStoneColor
+    );
+    this.io.to(roomId).emit("game_over", { winner: winner, how: "시간초과" });
     room.roomReset();
+    this.roomUserReset(roomId);
+    this.emitRoomInfo(roomId);
+  }
+  timeoutSetWinner(roomId: string) {
+    const room = this.getRoom(roomId);
+    const winner = room.currentTurn === "black" ? "white" : "black";
+    room.setWinner(winner);
+  }
+  roomUserReset(roomId: string) {
+    const room = this.getRoom(roomId);
+    room.users.forEach((user) => {
+      user.setIsReady(false);
+    });
   }
 }
--- socket.ts
+++ socket.ts
@@ -26,69 +26,107 @@ export default function initSocket(io: Server) {
 
     //방생성(클라이언트요청)
     socket.on("create_room", async (title: string) => {
-      const room = roomController.createRoom(title, socket, simpleUser);
+      try {
+        const room = roomController.createRoom(title, socket, simpleUser);
 
-      socket.emit("created_room", { success: true, roomId: room.roomId });
-      socket.emit("owner", room.getOwner());
+        socket.emit("created_room", { success: true, roomId: room.roomId });
+        socket.emit("owner", room.getOwner());
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //방입장(클라이언트요청)
     socket.on("join_room", async (roomId, callback) => {
-      const join = roomController.joinRoom(roomId, socket, simpleUser);
-      roomController.emitRoomInfo(roomId);
-      console.log("방입장의 join : ", join);
-      callback(join);
+      try {
+        const join = roomController.joinRoom(roomId, socket, simpleUser);
+        roomController.emitRoomInfo(roomId);
+        console.log("방입장의 join : ", join);
+        callback(join);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //방정보(클라이언트요청)
     socket.on("room_info", (roomId, callback) => {
-      const room = roomController.getRoom(roomId);
-      callback(room);
+      try {
+        const room = roomController.getRoom(roomId);
+        callback(room);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //채팅전송(클라이언트요청)
     socket.on("send_message", (data) => {
-      roomController.sendMessage(data, socket);
+      try {
+        roomController.sendMessage(data, socket);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //방나가기(클라이언트요청)
     socket.on("leave_room", (roomId) => {
-      roomController.leaveRoom(roomId, socket);
-      roomController.emitRoomInfo(roomId);
+      try {
+        roomController.leaveRoom(roomId, socket);
+        roomController.emitRoomInfo(roomId);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //방목록 불러오기(클라이언트요청)
     socket.on("get_room_list", () => {
-      roomController.getRoomList(socket);
+      try {
+        roomController.emitRoomList(socket);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //사용자 ready(클라이언트요청)
-    socket.on("ready", async (roomId, callback) => {
-      const ready = roomController.ready(roomId, simpleUser);
-
-      await userController.setGameStatusReady(username, ready);
-      callback(ready);
-      roomController.emitRoomInfo(roomId);
+    socket.on("ready", async (roomId) => {
+      try {
+        const ready = roomController.ready(roomId, simpleUser);
+        await userController.setGameStatusReady(username, ready);
+        roomController.emitRoomInfo(roomId);
+      } catch (error) {
+        console.log(error);
+      }
     });
 
     //방장이 게임 시작(클라이언트요청)
     socket.on("game_start", (roomId) => {
-      console.log("게임시작요청!");
-      roomController.gameStart(roomId);
+      try {
+        console.log("게임시작요청!");
+        roomController.gameStart(roomId, simpleUser);
+      } catch (error) {
+        console.log("game_start 오류", error);
+      }
     });
 
     //사용자가 돌 착수(클라이언트요청)
     socket.on("placed_stone", (roomId, data) => {
-      roomController.playerMove(roomId);
-      console.log(data, roomId);
-      io.to(roomId).emit("opponent_placed_stone", data);
+      try {
+        roomController.playerMove(roomId, simpleUser);
+        console.log(data, roomId);
+        io.to(roomId).emit("opponent_placed_stone", data);
+      } catch (error) {
+        console.log("placed_stone 오류", error);
+      }
     });
 
     //사용자 돌 색상 요청(클라이언트요청)
     socket.on("my_stone_color", (roomId, callback) => {
-      const room = roomController.getRoom(roomId);
-      const myColor = room.getMyColor(username);
-      callback(myColor);
+      try {
+        const room = roomController.getRoom(roomId);
+        const myColor = room.getMyColor(username);
+        callback(myColor);
+      } catch (error) {
+        console.log(error);
+      }
     });
   });
 }
